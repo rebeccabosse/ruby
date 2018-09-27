2c4b2da8e27789d69c835a7b129121c2129b4f20
rebeccabosse <rebeccabosse@hotmail.fr>
2018-09-28 01:33:32 +0200

    calcule des ages

---
+++ exo_10.rb
@@ -0,0 +1,9 @@
+puts "qu'elle est ta date de naissance?"
+user_name = gets.chomp
+
+now_years = 2017
+years_of_birth = 1991
+
+ age = years_of_birth + 1
+
+puts "mon age en 2017 : #{now_years - years_of_birth }"
