e1a26060ff6d4a936c4fa38cbcadcefb4151f4c8
rebeccabosse <rebeccabosse@hotmail.fr>
2018-09-28 02:09:03 +0200

    liste de mail

---
+++ exo_18.rb
@@ -0,0 +1,10 @@
+list =
+"beyonce.01@hotmail.fr\nladygaga.02@hotmail.fr\nr.kelly.03@hotmail.fr\nmadonna.04@hotmail.fr\nmikebrant.05@hotmail.fr\njustice.06@hotmail.fr\ngala.07@hotmail.fr\ndrake.08@hotmail.fr\nreas.09@hotmail.fr\nrihanna.10@hotmail.fr
+aqua.11@hotmail.fr\nqueens.12@hotmail.fr\ndido.13@hotmail.fr\nbritney.14@hotmail.fr\ntina.15@hotmail.fr\nwhitney.16@hotmail.fr\nchris.17@hotmail.fr\nlala.18@hotmail.fr\naya.19@hotmail.fr\nbosse.20@hotmail.fr
+sayan.21@hotmail.fr\niam.22@hotmail.fr\nntm.23@hotmail.fr\ndiams.24@hotmail.fr\nvita.25@hotmail.fr
+zazie.26@hotmail.fr\nthedo.27@hotmail.fr\nlenny.28@hotmail.fr\neminem.29@hotmail.fr\nninaj.30@hotmail.fr
+missy.31@hotmail.fr\nkathy.32@hotmail.fr\nlp.33@hotmail.fr\nbigbang.34@hotmail.fr\narashi.35@hotmail.fr\nsangoku.36@hotmail.fr\nvegeta.37@hotmail.fr
+trunks.38@hotmail.fr\nkrillin.39@hotmail.fr\nbroly.40@hotmail.fr\nchichi.41@hotmail.fr\nyamcha.42@hotmail.fr\nfreezer.43@hotmail.fr\nbulma.44@hotmail.fr\npan.45@hotmail.fr\nvidel.46@hotmail.fr\nhercule.47@hotmail.fr\nnaruto.48@hotmail.fr\nsasuke.49@hotmail.fr\nhokake.50@hotmail.fr\n"
+
+
+print list
