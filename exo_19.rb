98e99222575796787c39deb1f1d4392bf7621c7d
rebeccabosse <rebeccabosse@hotmail.fr>
2018-09-28 02:10:00 +0200

    afficher les bon mails

---
+++ exo_19.rb
@@ -0,0 +1,12 @@
+list =["beyonce.01@hotmail.fr","ladygaga.02@hotmail.fr","r.kelly.03@hotmail.fr","madonna.04@hotmail.fr","mikebrant.05@hotmail.fr","justice.06@hotmail.fr","gala.07@hotmail.fr","drake.08@hotmail.fr","reas.09@hotmail.fr","rihanna.10@hotmail.fr",
+"aqua.11@hotmail.fr","queens.12@hotmail.fr","dido.13@hotmail.fr","britney.14@hotmail.fr","tina.15@hotmail.fr","whitney.16@hotmail.fr","chris.17@hotmail.fr","lala.18@hotmail.fr","aya.19@hotmail.fr","bosse.20@hotmail.fr",
+"sayan.21@hotmail.fr","iam.22@hotmail.fr","ntm.23@hotmail.fr","diams.24@hotmail.fr","vita.25@hotmail.fr",
+"zazie.26@hotmail.fr","thedo.27@hotmail.fr","lenny.28@hotmail.fr","eminem.29@hotmail.fr","ninaj.30@hotmail.fr",
+"missy.31@hotmail.fr","kathy.32@hotmail.fr","lp.33@hotmail.fr","nbigbang.34@hotmail.fr","arashi.35@hotmail.fr","sangoku.36@hotmail.fr","vegeta.37@hotmail.fr",
+"trunks.38@hotmail.fr","krillin.39@hotmail.fr","broly.40@hotmail.fr","chichi.41@hotmail.fr","yamcha.42@hotmail.fr","freezer.43@hotmail.fr","bulma.44@hotmail.fr","pan.45@hotmail.fr","videl.46@hotmail.fr","hercule.47@hotmail.fr","naruto.48@hotmail.fr","sasuke.49@hotmail.fr","hokake.50@hotmail.fr"]
+    # Nous n'affichons que les indices pairs
+list.length.times do |i|
+    if i % 2 == 1
+        puts list[i]
+    end
+end
