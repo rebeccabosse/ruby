4282e42cbea22c673869e9dd32409f546f0a4895
rebeccabosse <rebeccabosse@hotmail.fr>
2018-09-28 02:08:26 +0200

    annoncer l'age, option bg

---
+++ exo_17.rb
@@ -0,0 +1,19 @@
+puts "entrez votre age:"
+age = gets.chomp.to_i
+
+
+age = age
+c = 1
+d = age - c
+
+loop do
+  puts " il y a #{c} ans, vous aviez #{age - c} ans "
+
+  break if c == age
+
+  c = c + 1
+if c == c
+puts "il y a #{d} ans, vous aviez #{d-c}ans soit la moitié de #{age} aujourd'hui"
+c = c + 1
+end
+end
